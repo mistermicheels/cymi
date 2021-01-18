8134b62b72746fb0476086b3b67517bed1c36eff
mistermicheels <22038058+mistermicheels@users.noreply.github.com>
2021-01-18 01:49:13 +0100

    show events with multiple responses only once

--- backend/src/main/java/com/mistermicheels/cymi/component/event/EventRepository.java
+++ backend/src/main/java/com/mistermicheels/cymi/component/event/EventRepository.java
@@ -12,9 +12,9 @@ interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT event FROM Event event JOIN FETCH event.group WHERE event.id = :id")
     Optional<Event> findWithGroupById(@Param(value = "id") Long id);
 
-    @Query("SELECT event FROM Event event LEFT JOIN FETCH event.responses WHERE event.group.id = :groupId AND event.endTimestamp > CURRENT_TIMESTAMP ORDER BY event.startTimestamp ASC")
+    @Query("SELECT DISTINCT event FROM Event event LEFT JOIN FETCH event.responses WHERE event.group.id = :groupId AND event.endTimestamp > CURRENT_TIMESTAMP ORDER BY event.startTimestamp ASC")
     List<Event> findUpcomingWithResponsesByGroupId(@Param(value = "groupId") Long groupId);
 
-    @Query("SELECT event FROM Event event JOIN FETCH event.group containingGroup JOIN containingGroup.members membership LEFT JOIN FETCH event.responses WHERE membership.groupUserLinkId.userId = :userId AND event.endTimestamp > CURRENT_TIMESTAMP ORDER BY event.startTimestamp ASC")
+    @Query("SELECT DISTINCT event FROM Event event JOIN FETCH event.group containingGroup JOIN containingGroup.members membership LEFT JOIN FETCH event.responses WHERE membership.groupUserLinkId.userId = :userId AND event.endTimestamp > CURRENT_TIMESTAMP ORDER BY event.startTimestamp ASC")
     List<Event> findUpcomingWithGroupWithResponsesByUserId(@Param(value = "userId") Long userId);
 }
